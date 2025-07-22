38d3e17cc7bf99112a0272d016f27f8069135e93
Max Ramon <MaxRamonNB@Gmail.com>
2025-07-22 12:25:02 -0300

    Acrescentando dados nas tabelas

--- Criação_de_Relatórios_SQL/Criação de Relatórios SQL.sql
+++ Criação_de_Relatórios_SQL/Criação de Relatórios SQL.sql
@@ -1,27 +1,27 @@
-USE insight_places; 
-CREATE TABLE proprietários (
-proprietarios_id VARCHAR (255) PRIMARY KEY, 
-nome VARCHAR (255),
-cpf_cnpj VARCHAR (20),
-contato VARCHAR (255)
-); 
+USE insight_place;
 
+CREATE TABLE proprietarios (
+proprietario_id VARCHAR(255) PRIMARY KEY,
+nome VARCHAR(255),
+cpf_cnpj VARCHAR(20),
+contato VARCHAR(255)
+);
 
 CREATE TABLE clientes (
-cliente_id VARCHAR (255) PRIMARY KEY, 
-nome VARCHAR (255),
-cpf VARCHAR (14),
-contato VARCHAR (255)
-); 
+    cliente_id VARCHAR(255) PRIMARY KEY,
+    nome VARCHAR(255),
+    cpf VARCHAR(14),
+    contato VARCHAR(255)
+);
 
 CREATE TABLE enderecos (
-endereco_id VARCHAR (255) PRIMARY KEY, 
-rua VARCHAR (255),
-numero INT, 
-bairro VARCHAR (255),
-cidade VARCHAR (255),
-estado VARCHAR (2),
-cep VARCHAR (10)
+    endereco_id VARCHAR(255) PRIMARY KEY,
+    rua VARCHAR(255),
+    numero INT,
+    bairro VARCHAR(255),
+    cidade VARCHAR(255),
+    estado VARCHAR(2),
+    cep VARCHAR(10)
 );
 
 CREATE TABLE hospedagens (
