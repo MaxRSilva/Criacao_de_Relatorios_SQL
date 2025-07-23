7621d43364ab6c01a17515276b285e0654586743
Max Ramon <MaxRamonNB@Gmail.com>
2025-07-23 11:26:50 -0300

    Verificação de propriedades inativas, por cliente

--- Criação_de_Relatórios_SQL/Analise dos dados.sql
+++ Criação_de_Relatórios_SQL/Analise dos dados.sql
@@ -31,3 +31,13 @@ WHERE h.ativo = 1
 GROUP BY p.nome 
 ORDER BY total_hospedagens_ativas DESC
 LIMIT 10; 
+
+-- Verificação de propriedades inativas 
+SELECT p.nome,	
+	COUNT(*) AS total_hospedagens_inativas
+FROM proprietarios p 
+JOIN hospedagens h 
+	ON p.proprietario_id = h.proprietario_id
+WHERE h.ativo = 0
+GROUP BY p.nome
+ORDER BY total_hospedagens_inativas DESC;
