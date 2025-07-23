4c47829454a6965d8130790e71de530ae1e0fe47
Max Ramon <MaxRamonNB@Gmail.com>
2025-07-23 11:01:43 -0300

    Informações de clientes e uso de alias

--- Criação_de_Relatórios_SQL/Analise dos dados.sql
+++ Criação_de_Relatórios_SQL/Analise dos dados.sql
@@ -10,3 +10,24 @@ SELECT
     ROUND (AVG (preco_total),2) AS 'ticket medio'
 FROM alugueis
 GROUP BY cliente_id;   
+
+-- Verificação da medida de estadia 
+
+SELECT 
+	cliente_id,
+    AVG(DATEDIFF(data_fim,data_inicio)) AS  media_dias_estadia
+FROM alugueis
+GROUP BY cliente_id
+ORDER BY media_dias_estadia DESC;
+
+
+-- Filtro de Informações dos proprietários
+SELECT p.nome AS nome_proprietário, 
+	COUNT(h.hospedagem_id) AS total_hospedagens_ativas 
+FROM proprietarios p 
+JOIN hospedagens h
+	ON p.proprietario_id = h.proprietario_id
+WHERE h.ativo = 1
+GROUP BY p.nome 
+ORDER BY total_hospedagens_ativas DESC
+LIMIT 10; 
