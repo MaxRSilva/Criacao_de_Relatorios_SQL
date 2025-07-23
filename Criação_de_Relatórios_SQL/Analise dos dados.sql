dc1b7b6fe9ed349719ba8a570cd9a30ddf3db97e
Max Ramon <MaxRamonNB@Gmail.com>
2025-07-23 10:42:36 -0300

    Analise da media de dias de estadia

--- Criação_de_Relatórios_SQL/Analise dos dados.sql
+++ Criação_de_Relatórios_SQL/Analise dos dados.sql
@@ -2,3 +2,11 @@
 SELECT * 
 FROM hospedagens
 WHERE tipo = 'hotel' AND ativo = 1;   
+
+
+-- Verificação de ticket médio dos clientes 
+SELECT
+	cliente_id,
+    ROUND (AVG (preco_total),2) AS 'ticket medio'
+FROM alugueis
+GROUP BY cliente_id;   
